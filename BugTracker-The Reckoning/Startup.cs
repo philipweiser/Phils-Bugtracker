efa7b8efadaca8827b8022282b8640de7a121839
philipweiser <philipkrw@gmail.com>
2015-02-16 11:14:29 -0500

    180

---
+++ BugTracker-The Reckoning/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BugTracker_The_Reckoning.Startup))]
+namespace BugTracker_The_Reckoning
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
