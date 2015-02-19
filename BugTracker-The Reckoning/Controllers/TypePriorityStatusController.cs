185919a27ed31742389c85517c75581a9ad6e4af
philipweiser <philipkrw@gmail.com>
2015-02-19 09:55:38 -0500

    TypesPrioritiesStatuses almost done

---
+++ BugTracker-The Reckoning/Controllers/TypePriorityStatusController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BugTracker_The_Reckoning.Models;
+using PagedList.Mvc;
+using PagedList;
+
+namespace BugTracker_The_Reckoning.Controllers
+{
+    [Authorize(Roles="Administrator, Project Manager")]
+    public class TypePriorityStatusController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: TypePriorityStatus
+        public ActionResult Index(int? typePage, int? statusPage, int? priorityPage)
+        {
+            var model = new TypePriorityStatus();
+
+            var pageNumber1 = typePage ?? 1;
+            model.types = db.TicketTypes.ToList().ToPagedList(pageNumber1, 5);
+
+            var pageNumber2 = statusPage ?? 1;
+            model.priorities = db.TicketPriorities.ToList().ToPagedList(pageNumber2, 5);
+
+            var pageNumber3 = priorityPage ?? 1;
+            model.statuses = db.TicketStatuses.ToList().ToPagedList(pageNumber3, 5);
+
+            ViewBag.typePage = pageNumber1;
+            ViewBag.statusPage = pageNumber2;
+            ViewBag.priorityPage = pageNumber3;
+            return View(model);
+        }
+    }
+}
---
+++ BugTracker-The Reckoning/Models/TypePriorityStatus.cs
@@ -0,0 +1,15 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTracker_The_Reckoning.Models
+{
+    public class TypePriorityStatus
+    {
+        public IPagedList<TicketPriority> priorities { get; set; }
+        public IPagedList<TicketStatus> statuses { get; set; }
+        public IPagedList<TicketType> types { get; set; }
+    }
+}
